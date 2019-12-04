9f5101a62787844cdeca06b34f05e9f46cf151cb
unknown <402418814@qq.com>
2019-12-04 10:27:46 +0800

    初始化提交

---
+++ application/config/wechat.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emotionalJim
+ * Date: 2019/12/2
+ * Time: 20:55
+ */
+
+// 公众号配置
+return [
+
+    // AppId
+    'AppId'               => "wx8498f7efc631cd22",
+    // AppSecret
+    'AppSecret'           => "b6f258cc3865650e484575406f2f9c19",
+
+];
