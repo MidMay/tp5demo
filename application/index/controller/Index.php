3d6867958629bf625c9d931c6955f3fcdc098da9
waterdance <402418814@qq.com>
2019-12-09 10:09:47 +0800

    伪静态修改：解决no input file问题

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,7 +1,15 @@
 <?php
 namespace app\index\controller;
 
-class Index
+use think\Controller;
+
+class Index extends Controller
 {
+    public function index(){
+        return 1;
+    }
+    public function login(){
+        return $this->fetch();
+    }
 
 }
